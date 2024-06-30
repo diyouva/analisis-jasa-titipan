902eaed074339e679e28b517943aff6558791f6d
diyouva <database.diyouva@gmail.com>
2024-07-01 02:14:46 +0700

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -17,7 +17,7 @@ st.write('Creating a Better CN Through Data')
 
 # Load data
 @st.cache_data
-def load_data():
+def load_main_data():
     df = pd.read_csv("data/cn1.csv")
     df = df.applymap(lambda x: x.strip() if isinstance(x, str) else x)
     df = df[df['HS_CODE'].replace('', np.nan).notna()]
@@ -25,19 +25,19 @@ def load_data():
 
 # Function
 ## Mencari Kemiripan Importir
-def search_importir(df):
+def search_importir(df, no_ident, nm_penerima, al_penerima):
     model_ident, vectorizer_ident, model_name, vectorizer_name, model_address, vectorizer_address, model_uraian, vectorizer_uraian = create_index(df)
     similar_id = find_similar(no_ident, nm_penerima, al_penerima, df, model_ident, vectorizer_ident, model_name, vectorizer_name, model_address, vectorizer_address)
     similarity_penerima = similar_id[similar_id['Similarity (%)'] > 60].head(10) # get similar_id that Similarity (%) > 0.6
     return similar_id, similarity_penerima, model_uraian, vectorizer_uraian   
 ## Mencari kesesuaian HS Code dengan Nama Produk
 filepath_produk = './data/hs_code_not_clean_id.csv'
-def load_data(filepath_produk, similar_id):
-    df_hs = pd.read_csv(filepath_produk, dtype=str)
+def load_data(filepath_produk, similar_id, sentence_model):
+    df_hs = load_secondary_data(filepath_produk, dtype=str)
     df_hs_results = get_similarity(similar_id, sentence_model, df_hs)
     return df_hs_results
 ## Mencari range harga berdasarkan uraian produk
-def range_harga(uraian_barang, df, model_uraian, vectorizer_uraian):
+def range_harga(uraian_barang, df, model_uraian, vectorizer_uraian, sentence_model):
     range_harga = get_range(uraian_barang, df, model_uraian, vectorizer_uraian, sentence_model)
     min_harga = range_harga[0]
     max_harga = range_harga[1]
@@ -52,13 +52,16 @@ uraian_barang = st.sidebar.text_input('URAIAN BARANG')
 
 #Predict button
 if st.sidebar.button('Predict'):
-    # Pencarian Importir
-    load_data()
-    search_importir()
+    # Load main data
+    df = load_main_data()
+
+    # Search for similar importers
+    similar_id, similarity_penerima, model_uraian, vectorizer_uraian = search_importir(df, no_ident, nm_penerima, al_penerima)
+
+    # Displaying results
     st.title('Displaying a Table in Streamlit')
     st.write('Here is a sample table:')
-    st.table(similarity_penerima)
-    # Mencari kesesuaian HS Code dengan Nama Produk
+    st.table(similarity_penerima)    # Mencari kesesuaian HS Code dengan Nama Produk
     
     # Mencari range harga berdasarkan uraian produk
 
